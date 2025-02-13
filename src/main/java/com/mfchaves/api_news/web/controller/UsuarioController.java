6e38bc49b3eb3f2a3288b5b3cee489c8bdf53b22
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-02-13 16:43:50 -0300

    Implementação do Mapper

--- src/main/java/com/mfchaves/api_news/web/controller/UsuarioController.java
+++ src/main/java/com/mfchaves/api_news/web/controller/UsuarioController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mfchaves.api_news.entity.Usuario;
 import com.mfchaves.api_news.service.UsuarioService;
+import com.mfchaves.api_news.web.dto.UsuarioResponseDto;
+import com.mfchaves.api_news.web.dto.mapper.UsuarioMapper;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
@@ -31,8 +33,8 @@ public class UsuarioController {
             @ApiResponse(responseCode = "200", description = "Lista com todos os usuários cadastrados", content = @Content(mediaType = "application/jason", array = @ArraySchema(schema = @Schema(implementation = Usuario.class))))
     })
     @GetMapping
-    public ResponseEntity<List<Usuario>> getAll() {
-        List<Usuario> usuarios = usuarioService.getAll();
+    public ResponseEntity<List<UsuarioResponseDto>> getAll() {
+        List<UsuarioResponseDto> usuarios = UsuarioMapper.toListDto(usuarioService.getAll());
         return ResponseEntity.ok(usuarios);
     }
 
